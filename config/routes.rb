81ce29bef1e72c3f29b9656613a6cbc856cd7d46
kenjitsumiyama <kenjitsumiyama0724@gmail.com>
2020-09-28 17:12:01 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "messages#index"
   get 'messages/index'
   resources :users, only: [:edit, :update]
-  
-  root to: "messages#index"
+  resources :rooms, only: [:new, :create]
 end
